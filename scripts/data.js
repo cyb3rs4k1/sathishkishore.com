e0c5add0a80f44f72ac4d31a7f1dd303949a5127
Sathish Kishore <chivaljazz@gmail.com>
2022-09-05 11:43:56 +0530

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,76 @@
+// Constants socials
+const linkedin = "https://www.linkedin.com/in/cyb3rs4k1/";
+const twitter = "https://twitter.com/cyb3rs4k1";
+const github = "https://github.com/cyb3rs4k1/";
+var today = new Date()
+
+// Intro (Who is cyb3rs4k1?)
+about = [
+    "<br>",
+    "Hey, I'm Sathish Kishore (cyb3rs4k1)",
+    "<br>",
+    "I am From Tamilnadu, Currently working as a Senior Security Engineer",
+    "with majors(Cyber Forensics) from University of Madras and Bachelors(B.E EEE) from Sathyabama university.",
+    "<br>",
+    "I used to play 3 instruments for fun, did a few stage shows. Practiced a few martial arts, won a silver medal state-level, and know to draw I guess, won state level prizes.",
+    "<br>",
+    "A pentesting veteran with hall of fames in Atos, Lumosity and 2 other private disclosures, not just web and mobile, but cloud, container, cms and red teaming as well",
+    "<br>",
+    "TLDR., I create develoeprs and Ops people problems, for a living.",
+    "<br>",
+    "I am currently working in threat modelling and DevSecOps implementation, with security hardening for all technologies and a few boring security audits.",
+    "<br>",
+    "I have experience in soc implementation with all sorts of monitoring enabled, with regex and YAML configurations and also post-forensic analysis using FastIR, Radare2, Ollydbg and autopsy",
+    "<br>",
+    "I play CTFs for fun, have a few certifications and also released two linux exploits which later got patched in linux 20.04 LTS",
+    "<br>",
+    "So not your average pentesting or a soc analyst mouse clicker.................",
+    "<br>"
+  ];
+
+// Banner
+banner = [
+    "<br>",
+    '<span class = "index">' + String(today) + '</span>',
+    '<span class="index">cyb3rs4k1    </span>',
+    "<br>",
+
+"░█████╗░██╗░░░██╗██████╗░██████╗░██████╗░░██████╗░░██╗██╗██╗░░██╗░░███╗░░",
+"██╔══██╗╚██╗░██╔╝██╔══██╗╚════██╗██╔══██╗██╔════╝░██╔╝██║██║░██╔╝░████║░░",
+"██║░░╚═╝░╚████╔╝░██████╦╝░█████╔╝██████╔╝╚█████╗░██╔╝░██║█████═╝░██╔██║░",
+"██║░░██╗░░╚██╔╝░░██╔══██╗░╚═══██╗██╔══██╗░╚═══██╗███████║██╔═██╗░╚═╝██║░░",
+"╚█████╔╝░░░██║░░░██████╦╝██████╔╝██║░░██║██████╔╝╚════██║██║░╚██╗███████╗",
+"░╚════╝░░░░╚═╝░░░╚═════╝░╚═════╝░╚═╝░░╚═╝╚═════╝░░░░░░╚═╝╚═╝░░╚═╝╚══════╝",
+ "<br>",
+
+    '<span class="color2">Welcome to my warzone</span>',
+    "<span class=\"color2\">For available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+    // '<span class="color2"></span>',
+    "<br>",
+]
+
+
+// Help Commands
+help = [
+    "<br>",
+    '👦🏻 <span class="command"> cyb3rs4k1</span>      About Me',
+    '🌏 <span class="command"> social</span>         Display social networks',
+    '❓ <span class="command"> help</span>           Open the help menu',
+    '🕰 <span class="command"> history</span>        View command history',
+    '🧹 <span class="command"> clear</span>          Clear the terminal',
+    '👋 <span class="command"> exit</span>           Close the tab and exit', 
+    "<br>",
+];
+
+// Social
+social = [
+    "<br>",
+    "Enter the Name of Platform or Just simply click on Username ...",
+    "<br>",
+    "|platform Name|   |Username|",
+    "<br>",
+    'linkedin          <a href="' + linkedin + '" target="_blank">cyb3rs4k1' + "</a>",
+    'github            <a href="' + github + '" target="_blank">cyb3rs4k1' + "</a>",
+    'twitter           <a href="' + twitter + '" target="_blank">cyb3rs4k1' + '</a>',
+    "<br>"
+];
